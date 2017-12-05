5d48366877fa5516031d52e96d15baea5b02775c
Hans Cornelissen <hans.cornelissen@tno.nl>
2017-12-05 16:59:25 +0100

    Update of arrow ie wind control: stop prop. of right cick to avoid context menu on map on go-live click on control

--- WebClient/Scripts/WindDirection.js
+++ WebClient/Scripts/WindDirection.js
@@ -1,20 +1,28 @@
+// environment control to show and set wind direction and speed
+// based on leaflet controls (no D3 used)
+// todo: add termperature
 
 function getDistance(draggable) {
+    // calculate location and angle relative to center of control based on mouse position during drag (leaflet class)
     var rect = draggable._dragStartTarget.getBoundingClientRect(),
         centerX = (rect.left + rect.right) / 2,
         centerY = (rect.top + rect.bottom) / 2,
-        offsetX = event ? event.clientX - centerX : draggable._lastEvent.clientX-centerX,
+        offsetX = event ? event.clientX - centerX : draggable._lastEvent.clientX - centerX,
         offsetY = event ? event.clientY - centerY : draggable._lastEvent.clientY - centerY;
 
     draggable.dx = offsetX * 2 / rect.width;
     draggable.dy = offsetY * 2 / rect.height;
+    // distance to center
     draggable.d = Math.sqrt(draggable.dx * draggable.dx + draggable.dy * draggable.dy);
+    // angle in degrees to center [0..360]
     draggable.r = (Math.atan2(draggable.dy, draggable.dx) * (180 / Math.PI) + 270) % 360;
-    draggable.s = Math.pow(Math.min(draggable.d/0.7, 1.0), 2.5) * draggable.ref.options.maxSpeed;
+    // speed, [0..1] relative to distance to center (1=0.7 of ellipse filling control) logarithmic scale (Niek)
+    draggable.s = Math.pow(Math.min(draggable.d / 0.7, 1.0), 2.5) * draggable.ref.options.maxSpeed;
+    // return the relative distance to center of control
     return draggable.d;
-};
+}
 
-
+// define control type
 L.Control.Arrow = L.Control.extend({
     arrow: null,
     text: null,
@@ -31,6 +39,7 @@ L.Control.Arrow = L.Control.extend({
         maxSpeed: 40 //in m/s
     },
 
+    /// leaflet constructor
     initialize: function (options) {
         L.setOptions(this, options);
     },
@@ -48,31 +57,40 @@ L.Control.Arrow = L.Control.extend({
     },
 
     _initLayout: function () {
+        // main div
         this._container = L.DomUtil.create('div', 'leaflet-control-windDirection');
+        // add drag support to main div
         this._draggable = new L.Draggable(this._container);
         this._draggable.ref = this;
         this._draggable._originalupdatePosition = this._draggable._updatePosition;
         this._draggable._updatePosition = function (e) {
-            // base on drag start handle drag of control, direction of wind or wind speed
+            // action is set in _dragdown
+            // check if we are ragging main div or are setting wind direction or speed
             if (this.action) {
-                //alert(this.action);
-                var d = getDistance(this);
-                //console.log(d + " angle: " + this.r);
-                switch (this.action) {
-                    case 1:
-                        this.ref.currentData.speed = this.s;
-                        this.ref.rotate(this.ref.currentData.direction, this.ref.currentData.speed);
-                        break;
-                    case 2:
-                        this.ref.currentData.direction = this.r;
-                        this.ref.rotate(this.ref.currentData.direction, this.ref.currentData.speed);
-                        break;
+                if (this.action > 0) {
+                    var d = getDistance(this);
+                    switch (this.action) {
+                        // setting speed
+                        case 1:
+                            this.ref.currentData.speed = this.s;
+                            this.ref.rotate(this.ref.currentData.direction, this.ref.currentData.speed);
+                            break;
+                        // setting wind direction
+                        case 2:
+                            this.ref.currentData.direction = this.r;
+                            this.ref.rotate(this.ref.currentData.direction, this.ref.currentData.speed);
+                            break;
+                    }
+                    // work-a-round: ensure dragend is fired!
+                    this._moved = true;
+                    this._moving = true;
                 }
-                // ensure dragend is fired!
-                this._moved = true;
-                this._moving = true;
+                else {
+                    
+                }
             }
             else {
+                // we are dragging the main div (control as a whole)
                 this._originalupdatePosition(e);
             }
         };
@@ -86,9 +104,9 @@ L.Control.Arrow = L.Control.extend({
 
         L.DomEvent.disableClickPropagation(this._container);
         L.DomEvent.disableScrollPropagation(this._container);
-
+        // use context menu event to return to "live" state
         this._container.addEventListener('contextmenu', this._handleContextMenu.bind(this));
-    
+
         this.text = this._container.appendChild(L.DomUtil.create('span', "windDirectionText"));
         this.text.innerHTML = "<BR>unknown<BR>";
         this._windDirectionDiv = L.DomUtil.create('div', "windDirectionDiv");
@@ -108,11 +126,15 @@ L.Control.Arrow = L.Control.extend({
         this.arrow.src = "Content/images/arrow_wind.png";
         this.arrow.id = "windArrow";
         L.DomUtil.addClass(this.arrow, 'windArrowLive');
+        // show initial direction and speed
         this.rotate(this.currentData.direction, this.currentData.speed);
     },
 
-    _handleContextMenu: function(e) {
-        // go live again
+    _handleContextMenu: function (e) {
+        e.preventDefault();
+        e.cancelBubble = true;
+        
+        // return to live state
         this.arrow.src = "Content/images/arrow_wind.png";
         wsSend({
             type: 'windData',
@@ -121,6 +143,8 @@ L.Control.Arrow = L.Control.extend({
     },
 
     _dragdown: function (e) {
+        // check if we are going to drag the main div or set the wind direction or set the wind speed
+        // set action accordingly to process action in 
         if (event && event.button != 2) {
             var d = getDistance(this);
             if (this.d < 1.0) {
@@ -150,12 +174,13 @@ L.Control.Arrow = L.Control.extend({
                 // nothing, just dragging
                 this.action = 0;
             }
-        } else {
-            
         }
+        else
+            this.action = -1;
     },
 
     _dragup: function (e) {
+        // end the drag action according to the previously set action
         switch (this._draggable.action) {
             case 1:
                 // final speed
@@ -181,15 +206,14 @@ L.Control.Arrow = L.Control.extend({
     },
 
     _update: function () {
+        // todo: needed?
     },
 
     NewData: (function (data) {
-        if (data.live) {
-            this.arrow.src = "Content/images/arrow_wind.png";
-        }
-        else {
-            this.arrow.src = "Content/images/arrow_wind_manual.png";
-        }
+        // set new state via websocket
+        // show new live state
+        this.arrow.src = data.live ? "Content/images/arrow_wind.png" : "Content/images/arrow_wind_manual.png";
+        // speed and/or direction
         var changed = false;
         if (typeof data.speed !== 'undefined' && data.speed != this.currentData.speed) {
             this.currentData.speed = data.speed;
@@ -199,13 +223,16 @@ L.Control.Arrow = L.Control.extend({
             this.currentData.direction = data.direction;
             changed = true;
         }
+        // show new state of speed and direction if any changed
         if (changed)
             this.rotate(this.currentData.direction, this.currentData.speed);
     }),
 
     rotate: function (degrees, speed) {
+        // show wind direction and speed
         degrees = Math.round(degrees * 100) / 100;
         speed = Math.round(speed * 100) / 100;
+        // according to position of wind vane position text with speed and direction over or below wind vane 
         if (degrees >= 270 || degrees <= 90) {
             this.text.style.top = '20%';
             this.text.style.removeProperty('bottom');
@@ -213,15 +240,14 @@ L.Control.Arrow = L.Control.extend({
             this.text.style.bottom = '20%';
             this.text.style.removeProperty('top');
         }
-        
-        //var arrow = document.getElementById("windArrow");
+        // update visual
         this.arrow.style.transform = "rotate(" + (degrees + 90) + "deg)";
         this.arrow.style.transform.webkitTransform = "rotate(" + (degrees + 90) + "deg)";
-
         this.text.innerHTML = degrees + '&deg;' + "<BR>" + speed + ' m/s';
     }
-})
+});
 
+// add arrow constructor for arrow control
 L.control.arrow = function () {
     return new L.Control.Arrow();
-}
+};
