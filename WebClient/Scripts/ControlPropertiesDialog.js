fdad1192eb4de1ccbd0c9d3330e4019a37378f01
Niek Knoop <nknoop@gmail.com>
2018-06-25 10:53:33 +0200

    Forgot 1 file for the previous commit

---
+++ WebClient/Scripts/ControlPropertiesDialog.js
@@ -0,0 +1,24 @@
+﻿ControlPropertiesDialog = function (data) {
+    //define object properties
+    this.dialog = {};
+
+    //define object function
+    //this.BuildDialog = function (data) {
+    //    var dialog = this.dialog = modalDialogCreate('Control properties', 'control properties');
+
+    //    var contentContainer = dialog.appendChild(document.createElement('div'));
+    //    contentContainer.className = 'ctrlPropContainer';
+
+    //    this.BuildNode(contentContainer, data);
+    //};
+
+    //this.BuildNode = function (container, node) {
+    //    var list = container.appendChild(document.createElement('ul'));
+    //    list.className = 'ctrlPropUL';
+
+    //    var titleLI = list.appendChild(document.createElement('li'));
+    //    titleLI.className = '';
+    //}
+
+    //initialize object
+};
